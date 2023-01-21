207710e4ce795cd05e2395626adb95948b6d0903
albertobregliano <alberto.bregliano@protonmail.com>
2023-01-21 14:27:03 +0100

    Examples

--- src/slice_test.go
+++ src/slice_test.go
@@ -7,14 +7,16 @@ import (
 )
 
 func ExampleFilter_int() {
-	s := slice.Filter([]int{0, 1, 2, 3, 4}, func(x int) bool { return x < 3 })
+	intSlice := []int{0, 1, 2, 3, 4}
+	s := slice.Filter(intSlice, func(x int) bool { return x < 3 })
 	fmt.Println(s)
 	// Output:
 	// [0 1 2]
 }
 
 func ExampleFilter_str() {
-	s := slice.Filter([]string{"zero", "one", "two", "three", "four"},
+	strSlice := []string{"zero", "one", "two", "three", "four"}
+	s := slice.Filter(strSlice,
 		func(x string) bool { return []rune(x)[0] == 't' })
 	fmt.Println(s)
 	// Output:
@@ -43,21 +45,24 @@ func ExampleReverse_int() {
 }
 
 func ExampleReverse_str() {
-	s := slice.Reverse([]string{"zero", "one", "two", "three", "four"})
+	strSlice := []string{"zero", "one", "two", "three", "four"}
+	s := slice.Reverse(strSlice)
 	fmt.Println(s)
 	// Output:
 	// [four three two one zero]
 }
 
 func ExampleInsert_str() {
-	s := slice.Insert([]string{"zero", "one", "two", "three", "four"}, 2, "two2", "three3")
+	strSlice := []string{"zero", "one", "two", "three", "four"}
+	s := slice.Insert(strSlice, 2, "two2", "three3")
 	fmt.Println(s)
 	// Output:
 	// [zero one two2 three3 two three four]
 }
 
 func ExampleInsert_int() {
-	s := slice.Insert([]int{0, 1, 2, 3, 4}, 2, 22, 33)
+	intSlice := []int{0, 1, 2, 3, 4}
+	s := slice.Insert(intSlice, 2, 22, 33)
 	fmt.Println(s)
 	// Output:
 	// [0 1 22 33 2 3 4]
@@ -71,7 +76,8 @@ func ExampleScramble_int() {
 }
 
 func ExampleScramble_str() {
-	s := slice.Scramble([]string{"zero", "one", "two", "three", "four"}, 1000)
+	strSlice := []string{"zero", "one", "two", "three", "four"}
+	s := slice.Scramble(strSlice, 1000)
 	fmt.Println(s)
 	// Output:
 	// [one three zero four two]
